131222e1dc739546d79840a483f9182d31e8eba5
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-24 16:34:14 -0600

    passed tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array, value) {
+
+    let args = [...arguments];
+    args.shift();
+    let argsLength = args.length;
+    console.log("args before loop is " + args);
+    console.log("args.length is " + argsLength)
+
+    let newArray = [""];
+    let arrayCounter = 0;
+
+    for (let i = 0; i < argsLength; i++) {
+        
+        console.log("args[i] is " + args[i]);
+        console.log("i is " + i);
+
+            for (let j = 0; j < (Array.length); j++) {
+                console.log("array[j] is " + Array[j]);
+                if (Array[j] === args[i]) {
+                    
+                    console.log("index is j: " + j);
+                    newArray = Array.splice(j, 1);
+                    arrayCounter += 1;
+                }
+
+                }
+
+         }
+   
+    console.log("items removed were: " + newArray);
+    return Array;
 
 };
 
