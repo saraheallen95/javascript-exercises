9f506b9fb532b3c8038a19764e0949f362f02dfd
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-24 15:37:47 -0600

    passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedArray = [""];
+    let stringArray = Array.from(string);
 
+    for (i = 1; i <= stringArray.length; i ++) {
+        reversedArray.push(stringArray[stringArray.length - i]);
+    }
+
+    let reversedString = reversedArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
