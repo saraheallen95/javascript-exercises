ff8bdc3a64e654a0b63de681ff78122506ff2f2e
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-24 15:10:09 -0600

    passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  
+    let returnString = "";
 
+    if (num > 0) {
+        for (i=0; i < num; i++){
+            returnString += string; 
+        /* console.log("returnString after adding is " + returnString);
+            console.log("num is" + i);
+            console.log("i is" + i);
+            console.log("string is" + string);*/
+        }
+
+        return returnString;
+    }
+
+    else if (num == 0) {
+        return returnString;
+    }
+
+    else return "ERROR";
 };
 
 // Do not edit below this line
