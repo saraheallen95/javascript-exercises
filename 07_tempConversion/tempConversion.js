9769a59747cc508b79cc4bf53a8795f9a12d8f99
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-25 11:56:24 -0600

    passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let c = 0;
+  c = (f - 32) * (5/9);
+  return Math.round(c*10)/10
 
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = 0;
+  f = (c * (9/5)) + 32;
+  return Math.round(f*10)/10;
 };
 
 // Do not edit below this line
