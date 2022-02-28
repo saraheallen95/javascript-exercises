04847e0ac30f69dade149109387dc08e46c15618
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-28 13:08:08 -0600

    passed tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function(word) {
+
+    let wordArray = word.split("");
+    let backwardsArray = [];
+    let wordLength = wordArray.length;
+    let lowerCasedWord = [];
+    lowerCasedWord = wordArray.map(wordArray => wordArray.toLowerCase())
+
+    for (let i = (wordArray.length - 1); i >= 0; i--) {
+
+        if (lowerCasedWord[i].toLowerCase() != lowerCasedWord[i].toUpperCase()
+        ) {
+            backwardsArray.push(lowerCasedWord[i]);
+
+        }
+
+        else {
+            lowerCasedWord.splice(i, 1);
+        }
+    }
+
+    if (backwardsArray.join() == lowerCasedWord.join()) {return true;}
+    else {return false};
 
 };
 
