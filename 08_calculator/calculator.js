0768fd2315a14492f0ecdd994405ddfa304f6b27
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-28 11:37:38 -0600

    passed tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,79 @@
-const add = function() {
+const add = function(x, y) {
+  let addResult = 0;
+  addResult = x + y;
+  return addResult;
 	
 };
 
-const subtract = function() {
-	
-};
+const subtract = function(x, y) {
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+	return (x - y);
 
 };
 
-const power = function() {
+const sum = function(x, y) {
+
+  let sumResult = 0;
+  let args = arguments[0].slice();
+  let argsLength = args.length;
+
+  for (let i = 0; i < argsLength; i++)
+    {
+      sumResult += args[i];
+    }
+    
+    return sumResult;
+
+};
+
+const multiply = function(x, y) {
+ 
+  let args = arguments[0].slice();
+  let xTimesY = args[0];
+  args.shift();
+  let argsLength = args.length;
+
+  for (let i = 0; i < argsLength; i++) {
+      xTimesY *= args[i];
+  }
+
+  return xTimesY;
+
+};
+
+const power = function(x, y) {
+
+  let xPowerY = x;
+
+  if (y == 0) {
+    return 1;
+  }
+ 
+  else {
+  for (i = 1; i < y; i++) {
+    xPowerY *= x;
+  }
+  return xPowerY;}
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+	let factorial = num;
+  let counter = num;
+
+  if (num != 0) {
+     for (i = 1; i < counter; i++) {
+     factorial = factorial * (num - 1);
+      num = num - 1;
+      }
+    return factorial;
+  }
+
+  else {
+    return 1;
+  }
+
 };
 
 // Do not edit below this line
