635f4148d473bd0c57059378279d4b7980e7b6d3
Sarah Allen <sarahallen1@Sarahs-MacBook-Air-2.local>
2022-02-28 16:31:59 -0600

    passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (number > 0) {
+        let fibonacciResult = 1;
+        let firstNum = fibonacciResult;
+
+        let arr = [1, 1];
+
+        for (let i = 0; i < number; i++)
+        {
+            /*console.log("number is " + number + "; i is " + i);*/
+            fibonacciResult = arr[i] + arr[i+1];
+            arr.push(fibonacciResult);
+            /*console.log("fibonacciResult is " + fibonacciResult);*/
+        }
+
+        /*console.log("arr is " + arr)*/
+        return arr[number - 1];
+    }
+
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
